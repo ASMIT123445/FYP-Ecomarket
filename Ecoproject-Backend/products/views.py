55827e0f08e0e5be47e05e85f3531aaa998c6c87
Asmit Pradhan <np03cs4a230138@heraldcollege.edu.np>
2025-11-09 23:28:24 +0545

    initial commit

---
+++ Ecoproject-Backend/products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+#  remove  down
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# @api_view(['GET'])
+# def test_api(request):
+#     data = {
+#         "message": "Backend is connected!",
+#         "status": "success"
+#     }
+#     return Response(data)
---
+++ React-Frontend/my-app/src/App.js
@@ -0,0 +1,24 @@
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
+
+//can remove
+// import TestBackend from "./components/TestBackend";
+
+// function App() {
+//   return (
+//     <div>
+//       <TestBackend />
+//     </div>
+//   );
+// }
+
+// export default App;
